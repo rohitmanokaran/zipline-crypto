2cadd4b6e1bba8a9eae2bbced1ff4844e56f2cb9
Rohit <rohit000@live.com>
2023-02-22 02:37:53 +0800

    Maybe fix wheel build

--- tools/install_talib.sh
+++ tools/install_talib.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
-curl -L -o ta-lib-0.4.0-src.tar.gz http://prdownloads.sourceforge.net/ta-lib/ta-lib-0.4.0-src.tar.gz &&
-  tar xvfz ta-lib-0.4.0-src.tar.gz &&
-  cd ta-lib &&
-  ./configure &&
-  make &&
-  make install &&
-  ldconfig
+# Test installing a newer version ta-lib via pip instead, since the later version is not on sourceforge
+
+#curl -L -o ta-lib-0.4.0-src.tar.gz http://prdownloads.sourceforge.net/ta-lib/ta-lib-0.4.0-src.tar.gz &&
+#  tar xvfz ta-lib-0.4.0-src.tar.gz &&
+#  cd ta-lib &&
+#  ./configure &&
+#  make &&
+#  make install &&
+#  ldconfig
